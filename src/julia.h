963735099bd9fd541a37fe30ce455c9f53ffedc6
Cody Tapscott <topolarity@tapscott.me>
2024-06-12 22:49:39 -0400

    Implement `jl_safe_static_show`
    This is similar to `jl_static_show` but is wrapped in a setjmp() for
    error tolerance.

--- src/julia.h
+++ src/julia.h
@@ -2460,6 +2460,7 @@ JL_DLLEXPORT int jl_termios_size(void);
 JL_DLLEXPORT void jl_flush_cstdio(void) JL_NOTSAFEPOINT;
 JL_DLLEXPORT jl_value_t *jl_stderr_obj(void) JL_NOTSAFEPOINT;
 JL_DLLEXPORT size_t jl_static_show(JL_STREAM *out, jl_value_t *v) JL_NOTSAFEPOINT;
+JL_DLLEXPORT size_t jl_safe_static_show(JL_STREAM *out, jl_value_t *v) JL_NOTSAFEPOINT;
 JL_DLLEXPORT size_t jl_static_show_func_sig(JL_STREAM *s, jl_value_t *type) JL_NOTSAFEPOINT;
 JL_DLLEXPORT void jl_print_backtrace(ios_t *s) JL_NOTSAFEPOINT;
 JL_DLLEXPORT void jlbacktrace(ios_t *s) JL_NOTSAFEPOINT; // deprecated
